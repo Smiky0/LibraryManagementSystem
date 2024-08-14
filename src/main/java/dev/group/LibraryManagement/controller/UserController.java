e50f8eb9d4c66d608a65e90d02d5880b913c90c9
Soumik <smik1172@gmail.com>
2024-08-14 14:31:25 +0530

    update: endpoint name changed

--- src/main/java/dev/group/LibraryManagement/controller/UserController.java
+++ src/main/java/dev/group/LibraryManagement/controller/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/users")
+@RequestMappig("/user")
 public class UserController {
 
     @Autowired
